69990e161f8f56086f75f4169037380ade87d4e6
Xavier <xluna600@gmail.com>
2020-06-04 17:59:11 +0000

    Add comment file to let users  add thier name and timestamp into the comment field

---
+++ portfolio/src/main/java/com/google/sps/data/Comments.java
@@ -0,0 +1,15 @@
+package com.google.sps.data;
+
+public final class Comments {
+    private final long id;
+    private final String name;
+    private final String comment;
+    private final long timestamp;
+
+    public Comments(long id, String name, String comment, long timestamp) {
+        this.id = id;
+        this.name = naem;
+        this.comment = comment;
+        this.timestamp = timestamp;
+    }
+}
